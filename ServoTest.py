08d3f55b37325b5de1ff44cffc373b250a4d307c
Martin <martincywong08@gmail.com>
2019-03-02 18:39:56 -0500

    change servo test sequence to be a function

--- ServoTest.py
+++ ServoTest.py
@@ -13,18 +13,21 @@ kit = ServoKit(channels=16)
 ##kit.continuous_servo[1].throttle = 0
 ##kit.continuous_servo[2].throttle = 0
 ##kit.continuous_servo[3].throttle = 0
-testServo = 5
-kit.servo[testServo].angle = 0
-time.sleep(1)
-kit.servo[testServo].angle = 45
-time.sleep(1)
-kit.servo[testServo].angle = 90
-time.sleep(1)
-kit.servo[testServo].angle = 135
-time.sleep(1)
-kit.servo[testServo].angle = 180
-time.sleep(1)
-kit.servo[testServo].angle = 110
-time.sleep(1)
-kit.servo[testServo].angle = 0
 
+def servoTest(int chID)
+    kit.servo[chID].angle = 0
+    time.sleep(1)
+    kit.servo[chID].angle = 45
+    time.sleep(1)
+    kit.servo[chID].angle = 90
+    time.sleep(1)
+    kit.servo[chID].angle = 135
+    time.sleep(1)
+    kit.servo[chID].angle = 180
+    time.sleep(1)
+    kit.servo[chID].angle = 110
+    time.sleep(1)
+    kit.servo[chID].angle = 0
+
+servoTest(4)
+servoTest(5)
