60ff4809fead89bd7458d42441d11886727d60eb
capsulemate <tnimalak@uwaterloo.ca>
2019-03-02 18:32:02 -0500

    testing 2 dispenser

--- ServoTest.py
+++ ServoTest.py
@@ -2,41 +2,29 @@ import time
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
-kit.continuous_servo[0].throttle = -0.25
-kit.continuous_servo[1].throttle = -0.25
-kit.continuous_servo[2].throttle = -0.25
-kit.continuous_servo[3].throttle = -0.25
-
-time.sleep(1.5)
-
-kit.continuous_servo[0].throttle = 0
-kit.continuous_servo[1].throttle = 0
-kit.continuous_servo[2].throttle = 0
-kit.continuous_servo[3].throttle = 0
-
-##kit.servo[4].angle = 0
+##kit.continuous_servo[0].throttle = -0.25
+##kit.continuous_servo[1].throttle = -0.25
+##kit.continuous_servo[2].throttle = -0.25
+##kit.continuous_servo[3].throttle = -0.25
 ##
-##time.sleep(1)
+##time.sleep(1.5)
 ##
-##kit.servo[4].angle = 45
-##
-##time.sleep(1)
-##
-##kit.servo[4].angle = 90
-##
-##time.sleep(1)
-##
-##kit.servo[4].angle = 135
-##
-##time.sleep(1)
-##
-##kit.servo[4].angle = 180
-##
-##time.sleep(1)
-##
-##kit.servo[4].angle = 110
-##
-##time.sleep(1)
-
-#kit.servo[4].angle = 0
+##kit.continuous_servo[0].throttle = 0
+##kit.continuous_servo[1].throttle = 0
+##kit.continuous_servo[2].throttle = 0
+##kit.continuous_servo[3].throttle = 0
+testServo = 5
+kit.servo[testServo].angle = 0
+time.sleep(1)
+kit.servo[testServo].angle = 45
+time.sleep(1)
+kit.servo[testServo].angle = 90
+time.sleep(1)
+kit.servo[testServo].angle = 135
+time.sleep(1)
+kit.servo[testServo].angle = 180
+time.sleep(1)
+kit.servo[testServo].angle = 110
+time.sleep(1)
+kit.servo[testServo].angle = 0
 
